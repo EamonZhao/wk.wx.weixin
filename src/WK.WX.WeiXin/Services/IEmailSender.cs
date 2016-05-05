2a7d2f087e95bb56292e2809c171ab7c9add9718
Eamon Zhao <eamon.zhao@outlook.com>
2016-05-05 21:07:10 +0800

    Init Project

---
+++ src/WK.WX.WeiXin/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WK.WX.WeiXin.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
