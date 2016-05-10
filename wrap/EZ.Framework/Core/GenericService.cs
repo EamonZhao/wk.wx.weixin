1b4e530ef0a4207f799304c97896828443cc2c54
赵旭明 <hhappyzxm@outlook.com>
2016-05-10 14:12:30 +0800

    Init Project

--- wrap/EZ.Framework/Core/GenericService.cs
+++ wrap/EZ.Framework/Core/GenericService.cs
@@ -2,11 +2,11 @@
 
 namespace EZ.Framework.Core
 {
-    public class GenericServiceAsync : IService
+    public class GenericService : IService
     {
         protected readonly IUnitOfWork UnitOfWork;
 
-        public GenericServiceAsync(IUnitOfWork unitOfWork)
+        public GenericService(IUnitOfWork unitOfWork)
         {
             if (unitOfWork == null) throw new ArgumentNullException(nameof(unitOfWork));
 
